40fcae00c2bb52c6bbdd8e7fdac406e0e6c495f2
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-10-17 19:26:25 +0630

    Runtime: 3 ms, faster than 93.27% of Java online submissions for Two Sum

---
+++ Easy/Twosum.java
@@ -0,0 +1,21 @@
+package Easy;
+import java.util.HashMap;
+public class Twosum {
+    public static void main(String[] args) {
+        int[] nums= {1,3,4,5};
+        int target=7;
+        HashMap<Integer,Integer> HM= new HashMap<>();
+        
+        for (int i =0 ; i<nums.length ; i++){
+           int comp=target-nums[i];
+            if(HM.containsKey(comp)){
+                // return new int[]{i,HM.get(comp)};
+                System.out.println(i + ","+ HM.get(comp));
+            }else {
+                HM.put(nums[i],i);
+            }
+            
+        }
+        //  return null; 
+    }
+}
