a7dc5b2407650431c611e3d4a04f4b5b8373efb8
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-06-10 15:52:59 +0530

    commit

--- patterns/Patterns.java
+++ patterns/Patterns.java
@@ -73,7 +73,56 @@ static void pattern7(int N)
         }
         System.out.println();
     }
+    
 } 
+static void pattern8(int N)
+{
+    for(int i=0;i<N;i++){
+        //spaces
+        for(int j=0;j<i;j++){
+            System.out.print(" ");
+        }
+        //starts
+        for(int j=0;j<2*N-(2*i+1);j++){
+            System.out.print("*");
+        }
+        //spaces
+        for(int j=0;j<i;j++){
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+    
+}
+static void pattern9(int N)
+{
+    for(int i=1;i<=2*N-1;i++){
+          int star= i;
+          if(i>N) star=2*N-i;
+          for(int j=1;j<=star;j++){
+            System.out.print("* ");
+          }
+        System.out.println();
+    }
+    
+}
+static void pattern10(int N)
+{
+    for(int i=0;i<N;i++){
+        int start=1;
+        if(i % 2==0){
+            start=1;
+        }else{
+            start=0;
+        }
+        for(int j=0;j<=i;j++){
+            System.out.print(start+" ");
+            start= 1-start;
+        }
+        System.out.println();
+    }
+    
+}
    public static void main(String[] args) {
     pattern1(4);
     pattern2(4);
@@ -82,6 +131,9 @@ static void pattern7(int N)
     pattern5(4);
     pattern6(4);
     pattern7(4);
+    pattern8(4);
+    pattern9(4);
+    pattern10(4);
 
     }
 }
