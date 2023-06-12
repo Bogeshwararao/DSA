d6f531a4e7e63e8d05b3c7cc45b9451af7e47358
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-06-12 10:53:27 +0530

    commit

--- patterns/Patterns.java
+++ patterns/Patterns.java
@@ -148,6 +148,31 @@ static void pattern11(int n){
    
     
 }
+static void pattern12(int n){
+    int space= 2*(n-1);
+    for(int i=1;i<=n;i++){
+        //number
+        for(int j=1;j<=i;j++){
+            System.out.print(j+" ");
+        }
+         //spaces
+         for(int j=1;j<=space;j++){
+             System.out.print(" ");
+         }
+         //number
+        for(int j=i;j>=1;j--){
+            System.out.print(j+" ");
+        }
+        
+        
+       
+        System.out.println();
+         space -=2;
+    }
+    
+   
+    
+}
    public static void main(String[] args) {
     pattern1(4);
     pattern2(4);
@@ -160,6 +185,6 @@ static void pattern11(int n){
     pattern9(4);
     pattern10(4);
     pattern11(4);
-    pattern11(4);
+    pattern12(4);
     }
 }
