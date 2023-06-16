5a4babcbce5ece4cd63ffa60a75b4d1363298851
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-06-17 04:52:57 +0530

    commit

--- Maths/Maths.java
+++ Maths/Maths.java
@@ -1,4 +1,15 @@
 package Maths;
 public class Maths {
-    
+   static void Countingdigits(int n){
+        int Count=0;
+        while(n> 0){
+            n /= 10;
+            Count =Count+1;
+            
+        }
+        System.out.println(Count);
+    }
+    public static void main(String[] args) {
+        Countingdigits(2004);
+    }
 }
