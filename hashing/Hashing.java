00cc6aba5190b162c61a55a07d0886f0880bfc29
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-06-20 18:00:26 +0530

    commit

--- hashing/Hashing.java
+++ hashing/Hashing.java
@@ -16,12 +16,24 @@ public class Hashing {
         System.out.println(set.size());
          Iterator itr = set.iterator();
         while (itr.hasNext()) {
-            System.out.print(itr.next());
+            System.out.print(itr.next()+" ");
         }
         
     }
-    static void Intersection(int arr1[], int arr2[]){
-         
+    static Integer Intersection(int arr1[], int arr2[]){
+        
+         HashSet<Integer> set=new HashSet<>();
+         int Count=0;
+         for(int i=0;i<arr1.length;i++){
+            set.add(arr1[i]);
+         }
+         for(int j=0;j<arr1.length;j++){
+            if(set.contains(arr2[j])){
+                Count =Count+1;
+                set.remove(arr2[j]);
+            }
+         }
+         return Count;
     }
     static void Majority(int arr[]){
 
@@ -49,10 +61,12 @@ public class Hashing {
 }
 public static void main(String[] args) {
     int arr[] = {10,5,10,15,10,5};
-     int arr1[] = {10,5,6,10,15,10,5};
-     int arr2[]={1,2,3};
+     int arr1[] = {10,5,6,15};
+     int arr2[]={1,2,3,5};
      int n = arr.length;
      Frequency(arr, n);
-     Union(arr1, arr2);
+    //  Union(arr1, arr2);
+    System.out.println(Intersection(arr1,arr2));
+    
 }
 }
