988513cf54ba277cebd6f92423aea9f6d31ecf8f
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-06-23 22:14:07 +0530

    commit

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -1,6 +1,12 @@
 package sorting;
 
 public class Sorting {
+    static void Print(int arr[],int n){
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
     static void Selection_sort(int arr[], int n){
         for (int i = 0; i < n - 1; i++) {
             int mini = i;
@@ -21,9 +27,23 @@ public class Sorting {
         }
         System.out.println();
     }
+    static void Bubble_sort(int arr[], int n){
+       for(int i=n-1;i>=0;i--){
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+        
+       }
+       Print(arr,n);
+    }
     public static void main(String[] args) {
         int arr[] = {13, 46, 24, 52, 20, 9};
         int n = arr.length;
-         Selection_sort(arr, n);
+        // Selection_sort(arr, n);
+         Bubble_sort(arr,n);
     }
 }
