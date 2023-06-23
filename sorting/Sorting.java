aba29f3940131e50df3e2ec2e898dd864cac53d0
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-06-23 19:49:53 +0530

    commit

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -1,5 +1,29 @@
 package sorting;
 
 public class Sorting {
-    
+    static void Selection_sort(int arr[], int n){
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            //swap
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("After selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+         Selection_sort(arr, n);
+    }
 }
