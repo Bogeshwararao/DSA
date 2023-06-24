56968b778c9cd8b9ded93e489e54b6f1c525bdf1
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-06-24 12:59:31 +0530

    commit

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -40,10 +40,24 @@ public class Sorting {
        }
        Print(arr,n);
     }
+    static void Insertion_sort(int arr[], int n){
+        for(int i=0;i<n-1;i++){
+            int j=i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+        System.out.println();
+        Print(arr,n);
+    }
     public static void main(String[] args) {
-        int arr[] = {13, 46, 24, 52, 20, 9};
+        int arr[] = {1, 46, 24, 52, 20, 9};
         int n = arr.length;
         Selection_sort(arr, n);
          Bubble_sort(arr,n);
+         Insertion_sort(arr,n);
     }
 }
